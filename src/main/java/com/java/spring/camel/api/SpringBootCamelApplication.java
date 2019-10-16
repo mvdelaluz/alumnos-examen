dad73f1a7bf9c907e09cbab6f5807ea710e5b8fc
vivichos <56464357+vivichos@users.noreply.github.com>
2019-10-16 08:47:29 -0500

    Update SpringBootCamelApplication.java

--- src/main/java/com/java/spring/camel/api/SpringBootCamelApplication.java
+++ src/main/java/com/java/spring/camel/api/SpringBootCamelApplication.java
@@ -2,7 +2,8 @@ package com.java.spring.camel.api;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+public static final String MY_DEFAULT_PROFILE = "myDefaultProfile";
+public static final String ACTIVEMQ = "activemq";
 
 @SpringBootApplication
 public class SpringBootCamelApplication extends RouteBuilder {
@@ -18,5 +19,28 @@ public void configure() throws Exception{
 	System.out.println("Termino de la transferencia a /home/viviandll/lab/evaluacion...");
 	
 };
+	
+@Bean
+CamelContextConfiguration contextConfiguration(@Value("${activemq.user}") String user,
+                                                   @Value("${activemq.password}") String password,
+                                                   @Value("${activemq.url}") String url) {
+        return new CamelContextConfiguration() {
 
+ @Override
+ public void beforeApplicationStart(CamelContext context) {
+                // your custom configuration goes here
+                ThreadPoolProfile threadPoolProfile = new ThreadPoolProfile();
+                threadPoolProfile.setId(MY_DEFAULT_PROFILE);
+                threadPoolProfile.setPoolSize(10);
+                threadPoolProfile.setMaxPoolSize(15);
+                threadPoolProfile.setMaxQueueSize(250);
+                threadPoolProfile.setKeepAliveTime(25L);
+                threadPoolProfile.setRejectedPolicy(ThreadPoolRejectedPolicy.Abort);
+                context.getExecutorServiceManager().registerThreadPoolProfile(threadPoolProfile);
+                ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(user, password, url);
+                context.addComponent(ACTIVEMQ, jmsComponentAutoAcknowledge(connectionFactory));
+            }
+        };
+
+}
 }
